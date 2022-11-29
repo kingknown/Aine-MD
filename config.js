826650afc4cf4e475715740d31ef6029d7a3a9ba
kingknown <118806215+kingknown@users.noreply.github.com>
2022-11-29 09:36:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6285797375014'],
+  ['6285797375014'],
+  ['6285797375014', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
